7f033fe54598e2a5dcf6a61271e9854a0b3b1ad8
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-06-04 16:14:00 +0800

    feat: add Java SDK and devops CLI

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# tutorial-java
+# Java
+
 Tutorial and training source code about Java.
+
+## 專案操作
+
++ 啟動
+
+使用 CLI 呼叫 docker-compose 來啟動相關服務
+
+```
+dev [java | maven | gradle]
+```
+
+依據選擇的項目會啟動不同的容器環境：
+
++ ```dev java``` 會啟動僅有 OpenJDK 的環境
++ ```dev maven``` 會啟用有 OpenJDK 與開發工具 Maven 的環境
++ ```dev gradle``` 會啟用有 OpenJDK 與開發工具 Gradle 的環境
+
+
+## 文獻
+
++ 環境
+    - [openjdk - Docker](https://hub.docker.com/_/openjdk)
+    - [Apache Maven - Docker](https://hub.docker.com/_/maven)
+    - [Gradle - Docker](https://hub.docker.com/_/gradle)
++ 教學
+    - [Java - w3schools](https://www.w3schools.com/java/)
++ Java project management tools
+    - [Apache Maven](https://maven.apache.org/)
+    - [Gradle](https://gradle.org/)
---
+++ conf/devops/maven.bat
@@ -0,0 +1,53 @@
+@rem ------------------- batch setting -------------------
+@echo off
+
+@rem ------------------- declare variable -------------------
+
+@rem ------------------- execute script -------------------
+call :%*
+goto end
+
+@rem ------------------- declare function -------------------
+
+:up-prepare (
+    @rem Create .env for compose
+    echo Current Environment %PROJECT_ENV%
+    echo PROJECT_NAME=%PROJECT_NAME% > %CONF_FILE_PATH%
+
+    @rem Application directory
+    echo APP_DIR=%CLI_DIRECTORY%app > %CONF_FILE_PATH%
+
+    @rem Cache directory
+    set TARGET_DIR=%CLI_DIRECTORY%cache
+    IF NOT EXIST %TARGET_DIR% (mkdir %TARGET_DIR%)
+    echo CACHE_DIR=%TARGET_DIR% >> %CONF_FILE_PATH%
+
+    goto end
+)
+
+:action
+    echo ^> Server startup
+    @rem Execute prepare action
+    call :up-prepare
+
+    @rem Run next deveopment with stdout
+    docker-compose -f .\conf\docker\docker-compose.yml --env-file %CONF_FILE_PATH% run --build --rm maven bash
+    goto end
+
+:args
+    goto end
+
+:short
+    echo Startup Server
+    goto end
+
+:help
+    echo This is a Command Line Interface with project %PROJECT_NAME%
+    echo Startup Server
+    echo.
+    echo Options:
+    echo      --help, -h        Show more information with '%~n0' command.
+    call %CLI_SHELL_DIRECTORY%\utils\tools.bat command-description %~n0
+    goto end
+
+:end
